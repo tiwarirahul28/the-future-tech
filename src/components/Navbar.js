3d873d7177d246315b87f3255629dd2bb4d28c95
Rahul Tiwari <tiwarirahul28042002@gmail.com>
2023-02-07 17:52:26 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
         <>
         <div className={color ? "header header-bg" : "header"}>
             <Link to="/">
-                <h1>Portfolio</h1>
+                <h1>The Future Tech</h1>
             </Link>
             <ul className={click ? "nav-menu active" : "nav-menu"}>
                 <li>
