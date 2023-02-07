53257aaf2cac752fac8cf5213593d9fbd44e6109
Rahul Tiwari <tiwarirahul28042002@gmail.com>
2023-02-07 15:15:19 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     const handleClick = () => setClick(!click);
     const [color, setColor] = useState(false);
     const changeColor = () => {
-        if(window.scrollY >= 100){
+        if(window.scrollY >= 10){
             setColor(true);
         }else{
             setColor(false);
