a88fda10c9e5dc32bdc4e03d2460be0d477b8f28
Rahul Tiwari <tiwarirahul28042002@gmail.com>
2023-02-10 23:43:49 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './FooterStyle.css'
+import Logo from '../assets/images/logo.png'
+import {BsTelephone} from 'react-icons/bs'
 
 const Footer = () => {
     return (
@@ -7,29 +9,42 @@ const Footer = () => {
         <div className="footer">
             <div className="box-container">
                 <div className="box">
-                    <h1>Contact us</h1>
-                    <h3>Rahul Tiwari | Tech Head</h3>
-                    <p>+91 9324272772</p> 
-                    <h3>Abhay Vishwakarma | Event Manager</h3>
-                    <p>+91 7738913369</p> 
-                    <h3>Mihir Salgaokar | Event Manager</h3>
-                    <p>+91 8779330699</p> 
+                    <img src={Logo} alt='logo'/>
+                    <p>The Future Tech Is An Inter-Collegiate IT Event Organized By The Students Of Bachelor Of Science In Information Technology (B.Sc-I.T.) For The Purpose Of Providing A Platform For Students To Showcase Their Skills And Knowledge In Various Tech Related Competitions.</p>
+                    <a rel="noreferrer" href="https://forms.gle/TvvUDAobVyMZaxmB8" target="_blank" className='btn footer-btn'>Register Link</a>
                 </div>
                 <div className="box">
-                    <h3>quick links</h3>
-                    <a href="/">home</a>
-                    <a href="/events">Events</a>
-                    <a href="/teams">Teams</a>
-                    <a href="/about">About</a>
+                    <h2>Contact us</h2>
+                    <div className='footer-contact'>
+                        <h3>Rahul Tiwari | Tech Head</h3>
+                        <a href='/'>
+                            <BsTelephone size={20} style={{color: "#E62429"}}/>+91 9324272772
+                        </a> 
+                        <h3>Abhay Vishwakarma | Event Manager</h3>
+                        <a href='/'>
+                            <BsTelephone size={20} style={{color: "#E62429"}}/>+91 7738913369
+                        </a> 
+                        <h3>Mihir Salgaokar | Event Manager</h3>
+                        <a href='/'>
+                            <BsTelephone size={20} style={{color: "#E62429"}}/>+91 8779330699
+                        </a> 
+                    </div>
                 </div>
                 <div className="box">
-                    <h3>Register Now</h3>
-                    <a rel="noreferrer" href="https://forms.gle/TvvUDAobVyMZaxmB8" target="_blank">Register Link</a>
+                    <h2>quick links</h2>
+                    <div className='footer-link'>
+                        <a href="/">home</a>
+                        <a href="/events">Events</a>
+                        <a href="/teams">Teams</a>
+                        <a href="/about">About</a>
+                    </div>
                 </div>
                 
                 <div className="box">
-                    <h3>follow us</h3>
-                    <a rel="noreferrer" href="https://www.instagram.com/the_future_tech_23/" target="_blank">instagram</a>
+                    <h2>follow us</h2>
+                    <div className='footer-insta'>
+                        <a rel="noreferrer" href="https://www.instagram.com/the_future_tech_23/" target="_blank">instagram</a>
+                    </div>
                 </div>
             </div>
             <h1 className="credit"> Created By <a href="/">KES STUD</a> 2023</h1>
