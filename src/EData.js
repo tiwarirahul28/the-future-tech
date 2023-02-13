24edb04e4d1f9a64a3a4a33c9a9879e7af65b7fd
Rahul Tiwari <tiwarirahul28042002@gmail.com>
2023-02-13 23:10:25 +0530

    first commit

--- src/EData.js
+++ src/EData.js
@@ -6,27 +6,27 @@ const EData = [
     {
         imgsrc: Debug,
         name: "Debug me",
-        rules: "The number of participants :01", 
-        rulesone: "This event consists of two rounds.", 
-        rulestwo: "In the following programming language, participants will be given code to debug.",
-        rulesthree: "Language: Javascript, C++, Java, Python"
+        rules: "1. The number of participants :01", 
+        rulesone: "2. This event consists of two rounds.", 
+        rulestwo: "3. In the following programming language, participants will be given code to debug.",
+        rulesthree: "4. Language: Javascript, C++, Java, Python"
     },
     {
         imgsrc: ai,
         name: "ARTIFICIAL INTELLIGENCE",
-        rules: "Students can participate solo or in teams (4 members).", 
-        rulesone:"Only 1 member to register from each team in the given google form.",
-        rulestwo:"This event consist of two round.",
-        rulesthree:"In ronud one, the participants make presentations and present them to the judges.",
-        rulesfour: "A second round will take place in which the participants present their implemented projects to the judges."
+        rules:"1. Students can participate solo or in teams (4 members).", 
+        rulesone:"2. Only 1 member to register from each team in the given google form.",
+        rulestwo:"3. This event consist of two round.",
+        rulesthree:"4. In ronud one, the participants make presentations and present them to the judges.",
+        rulesfour: "5. A second round will take place in which the participants present their implemented projects to the judges."
     },
     {
         imgsrc: AndroidApplication,
         name: "ANDROID APPLICATION ",
-        rules: "Students can participate in solo or teams(4 members).", 
-        rulesone:"Participants have make a Android application from scratch in our premises.",
-        rulestwo:"In ronud one, the participants make presentations and present them to the judges.",
-        rulesthree:"A second round will take place in which the participants present their implemented projects to the judges."
+        rules: "1. Students can participate in solo or teams(4 members).", 
+        rulesone:"2. Participants have make a Android application from scratch in our premises.",
+        rulestwo:"3. In ronud one, the participants make presentations and present them to the judges.",
+        rulesthree:"4. A second round will take place in which the participants present their implemented projects to the judges."
     },
 ]
 export default EData
