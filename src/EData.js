82198354e0ede4f96bb8172a9eb7917bdf23e405
Rahul Tiwari <tiwarirahul28042002@gmail.com>
2023-02-14 13:24:23 +0530

    first commit

--- src/EData.js
+++ src/EData.js
@@ -6,27 +6,26 @@ const EData = [
     {
         imgsrc: Debug,
         name: "Debug me",
-        rules: "1. The number of participants :01", 
-        rulesone: "2. This event consists of two rounds.", 
-        rulestwo: "3. In the following programming language, participants will be given code to debug.",
-        rulesthree: "4. Language: Javascript, C++, Java, Python"
+        rules:"1. The number of participants :01", 
+        rulesone:"2. No:of Rounds:02", 
+        rulestwo:"3. Participants will be given code to debug.",
+        rulesthree:"4. Programing Languages : Javascript, C++, Python."
     },
     {
         imgsrc: ai,
         name: "ARTIFICIAL INTELLIGENCE",
-        rules:"1. Students can participate solo or in teams (4 members).", 
+        rules: "1. Students can participate SOLO or in TEAM(4 members).", 
         rulesone:"2. Only 1 member to register from each team in the given google form.",
-        rulestwo:"3. This event consist of two round.",
-        rulesthree:"4. In ronud one, the participants make presentations and present them to the judges.",
-        rulesfour: "5. A second round will take place in which the participants present their implemented projects to the judges."
+        rulestwo:"3. No:of Rounds:02",
+        rulesthree:"4. In round 1, the participants has to make the presentation and present it to the judges.",
+        rulesfour: "5. In round 2 ,the participants will present their implemented projects to the judges"
     },
     {
         imgsrc: AndroidApplication,
         name: "ANDROID APPLICATION ",
-        rules: "1. Students can participate in solo or teams(4 members).", 
-        rulesone:"2. Participants have make a Android application from scratch in our premises.",
-        rulestwo:"3. In ronud one, the participants make presentations and present them to the judges.",
-        rulesthree:"4. A second round will take place in which the participants present their implemented projects to the judges."
+        rules: "1. Students can participate SOLO or in TEAM(4 members).", 
+        rulesone:"2. In round 1, the participants has to make the presentation and present it to the judges.",
+        rulestwo:"3. In round 2 ,the participants will present their implemented projects to the judges",
     },
 ]
 export default EData
