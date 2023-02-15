633bb7980ad76fd9179a28926c55a3fc779c6594
Rahul Tiwari <tiwarirahul28042002@gmail.com>
2023-02-15 12:16:16 +0530

    first commit

--- src/components/TeamName.js
+++ src/components/TeamName.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './TeamNameStyle.css'
-import Rahul from '../assets/images/team/rahul.jpg'
+// import Rahul from '../assets/images/team/rahul.jpg'
 import SonalMam from '../assets/images/team/sonal-mam.jpg'
 import ManjuMan from '../assets/images/team/manju-mam.jpeg'
 import Abhay from '../assets/images/team/abhay.jpeg'
 import Mihir from '../assets/images/team/mihir.jpg'
 import Zohab from '../assets/images/team/zohab.jpeg';
 import Arsalan from '../assets/images/team/arsalan.jpg';
-import Adib from '../assets/images/team/adib.jpg';
+// import Adib from '../assets/images/team/adib.jpg';
 const TeamName = () => {
     return (
         <>
@@ -24,63 +24,37 @@ const TeamName = () => {
                     <h2>advisor</h2>
                 </div>
                 <div className='team-box'>
-                    <img src={Rahul} alt=""/>
-                    <h1>Rahul Tiwari</h1>
+                    <img src={Mihir} alt=""/>
+                    <h1>Mihir Salgaonkar</h1>
                     <h2>Event Head</h2>
-                    <a href='tel:+91 9324272772'>+91 9324272772</a>
-                </div>
-                <div className='team-box'>
-                    <img src={Arsalan} alt=""/>
-                    <h1>arsalan shaikh</h1>
-                    <h2>Event Manager</h2>
-                    <a href='tel:+91 8291799787'>+91 8291799787</a>
-                </div>
-                <div className='team-box'>
-                    <img src={Zohab} alt=""/>
-                    <h1>abu zohab shaikh</h1>
-                    <h2>Event Manager</h2>
-                    <a href='tel:+91 9930863310'>+91 9930863310</a>
-                </div>
-            </div>
-            <h1 className='heading'>HOD (Head Of Department)</h1>
-            <div className='team-container'>
-                <div className='team-box'>
-                    <img src={Rahul} alt=""/>
-                    <h1>Rahul Tiwari</h1>
-                    <h2>Debugging (HOD)</h2>
-                    <a href='tel:+91 9324272772'>+91 9324272772</a>
-                </div>
-                <div className='team-box'>
-                    <img src={Arsalan} alt=""/>
-                    <h1>arsalan shaikh</h1>
-                    <h2>E-Treasure Hunt (HOD)</h2>
-                    <a href='tel:+91 8291799787'>+91 8291799787</a>
-                </div>
-                <div className='team-box'>
-                    <img src={Zohab} alt=""/>
-                    <h1>abu zohab shaikh</h1>
-                    <h2>Web Desigining & Blind Coding (HOD)</h2>
-                    <a href='tel:+91 9930863310'>+91 9930863310</a>
+                    <a href='tel+91 8779330699'>+91 8779330699</a>
                 </div>
                 <div className='team-box'>
                     <img src={Abhay} alt=""/>
                     <h1>Abhay Vishwakarma</h1>
-                    <h2>AI & IOT (HOD)</h2>
-                    <a href='tel:+91 7738913369'>+91 7738913369</a>
+                    <h2>Event Head</h2>
+                    <a href='tel+91 7738913369'>+91 7738913369</a>
                 </div>
                 <div className='team-box'>
-                    <img src={Mihir} alt=""/>
-                    <h1>Mihir Salgaonkar</h1>
-                    <h2>Android Application & Ethical Hacking (HOD)</h2>
-                    <a href='tel+91 8779330699'>+91 8779330699</a>
+                    <img src={Arsalan} alt=""/>
+                    <h1>arsalan shaikh</h1>
+                    <h2>Event Manager</h2>
+                    <a href='tel:+91 8291799787'>+91 8291799787</a>
                 </div>
                 <div className='team-box'>
-                    <img src={Adib} alt=""/>
-                    <h1>Adib Khan</h1>
-                    <h2>Call Of Duty (HOD)</h2>
-                    <a href='tel+91 8104397990'>+91 8104397990</a>
+                    <img src={Zohab} alt=""/>
+                    <h1>abu zohab shaikh</h1>
+                    <h2>Event Manager</h2>
+                    <a href='tel:+91 9930863310'>+91 9930863310</a>
                 </div>
+                {/* <div className='team-box'>
+                    <img src={Rahul} alt=""/>
+                    <h1>Rahul Tiwari</h1>
+                    <h2>Teach Head</h2>
+                    <a href='tel:+91 9324272772'>+91 9324272772</a>
+                </div> */}
             </div>
+                
         </section>
         </>
     )
