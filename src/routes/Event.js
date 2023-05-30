ba49a58beb7fd920e5a8406bca84de60e64aeecd
tiwarirahul28 <tiwarirahul09072019@gmail.com>
2023-05-30 22:29:53 +0530

    New Section

--- src/routes/Event.js
+++ src/routes/Event.js
@@ -3,7 +3,7 @@ import BannerImg from '../components/BannerImg'
 import EventName from '../components/EventName'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import EventScheduleList from '../components/Schedule/EventScheduleList'
+// import EventScheduleList from '../components/Schedule/EventScheduleList'
 
 const Event = () => {
     return (
@@ -14,7 +14,7 @@ const Event = () => {
             text="Events"
         />
         <EventName/>
-        <EventScheduleList/>
+        {/* <EventScheduleList/> */}
         <Footer/>
         </>
     )
